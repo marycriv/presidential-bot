2d1d982fdceb81213a13a500f7b17b381270793b
marycriv <maryecrivelli@gmail.com>
2019-07-05 10:56:58 -0400

    Added phrases

--- presidentBot.js
+++ presidentBot.js
@@ -10,9 +10,11 @@ const config = {
 
 const Twitter = new twit(config);
 
-const header = ["My fellow Americans,", "Everyone,", "", "", "" ]
-const body = ["today is a sad day for America.", "today will go down in history.", "today, we celebrate our country."]
-const footer = ["Thank you for your service.", "Truth, justice, and the American way.", "I'm proud to be an American."]
+const header = ["My fellow Americans,", "Everyone,", "Citizens of these United States,", "Brothers and sisters,", "If there's one thing I believe, it's that" ]
+
+const body = ["today is a sad day for America.", "today will go down in history.", "today, we celebrate our country.", "I am not afraid to say that I am proud to be an American.", "I believe in the power of the American public.", "America's economy is thriving.", "a job for every American citizen is a must.", "America is the land of the free.", "America is the home of the brave.", "we all deserve a hot burger and a cold beer at the end of the day.", "this land is your land, this land is my land."]
+
+const footer = ["Thank you for your service.", "Truth, justice, and the American way.", "I'm proud to be an American.", "Long may our banner wave.", "From sea to shining sea."]
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -21,9 +23,9 @@ function getRandomInt(min, max) {
 }
 
 let tweet = function() {
-  const h = getRandomInt(0, header.length)
-  const b = getRandomInt(0, body.length)
-  const f = getRandomInt(0, footer.length)
+  let h = getRandomInt(0, header.length)
+  let b = getRandomInt(0, body.length)
+  let f = getRandomInt(0, footer.length)
   Twitter.post('statuses/update', {
     status: header[h] + " " + body[b] + " " + footer[f]
   }, function(err, response) {
@@ -35,6 +37,9 @@ let tweet = function() {
     }
   });
 }
+
+tweet();
+setInterval(tweet, 120000);
 //
 // function tweetMaker() {
 //   fetch('/scripts.json')
@@ -45,6 +50,3 @@ let tweet = function() {
 //     })
 //   })
 // }
-
-tweet();
-setInterval(tweet, 600000);
